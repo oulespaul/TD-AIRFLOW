be45e9e6c17bae9e5b2b428f0e0a385132999a60
TANAKRIT CHAUKAJUNG <>
2022-09-23 17:40:44 +0700

    feat(all): add store to hdfs (redundant)

--- dags/building_plan.py
+++ dags/building_plan.py
@@ -24,9 +24,9 @@ dag = DAG('BUILDING_PLAN',
 def store_to_hdfs(**kwargs):
     hdfs = PyWebHdfsClient(host=Variable.get("hdfs_host"),
                            port=Variable.get("hdfs_port"), user_name=Variable.get("hdfs_username"))
-    
+
     ingest_date = datetime.now(tz=tzInfo)
-    my_dir = kwargs['directory'] + "/" +ingest_date.strftime("%Y%m%d")
+    my_dir = kwargs['directory'] + "/" + ingest_date.strftime("%Y%m%d")
     hdfs.make_dir(my_dir)
     hdfs.make_dir(my_dir, permission=755)
 
@@ -45,6 +45,32 @@ def store_to_hdfs(**kwargs):
     pprint("Ingestion done!")
     pprint(hdfs.list_dir(my_dir))
 
+
+def store_to_hdfs_for_redundant(**kwargs):
+    hdfs = PyWebHdfsClient(host=Variable.get("hdfs_host_redundant"),
+                           port=Variable.get("hdfs_port_redundant"), user_name=Variable.get("hdfs_username_redundant"))
+
+    ingest_date = datetime.now(tz=tzInfo)
+    my_dir = kwargs['directory'] + "/" + ingest_date.strftime("%Y%m%d")
+    hdfs.make_dir(my_dir)
+    hdfs.make_dir(my_dir, permission=755)
+
+    path = "/opt/airflow/ImagePool/Standard Pattern"
+
+    for subdir, dirs, files in os.walk(path):
+        pprint(f"Floder {subdir} ingesting...")
+        for file in files:
+            file_path = os.path.join(path, subdir, file)
+            with open(file_path, 'rb') as file_data:
+                my_data = file_data.read()
+                hdfs.create_file(my_dir+f"/{file}", my_data, overwrite=True)
+
+        pprint(f"Floder {subdir} ingestion done")
+
+    pprint("Ingestion done!")
+    pprint(hdfs.list_dir(my_dir))
+
+
 with dag:
     load_to_hdfs = PythonOperator(
         task_id='load_to_hdfs',
@@ -52,5 +78,10 @@ with dag:
         op_kwargs={'directory': '/data/raw_zone/building_plan'},
     )
 
+    load_to_hdfs_for_redundant = PythonOperator(
+        task_id='load_to_hdfs_for_redundant',
+        python_callable=store_to_hdfs_for_redundant,
+        op_kwargs={'directory': '/data/raw_zone/building_plan'},
+    )
 
-load_to_hdfs
+load_to_hdfs >> load_to_hdfs_for_redundant
