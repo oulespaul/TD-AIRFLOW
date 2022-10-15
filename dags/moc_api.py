f5ddfa956077620f8051e5205fd306382351044f
TANAKRIT CHAUKAJUNG <>
2022-10-15 13:39:49 +0700

    feat(dag): add full config in moc_api DAG

--- dags/moc_api.py
+++ dags/moc_api.py
@@ -91,22 +91,28 @@ def store_to_hdfs_for_redundant(**kwargs):
                            port=Variable.get("hdfs_port_redundant"), user_name=Variable.get("hdfs_username_redundant"))
 
     ingest_date = datetime.now(tz=tzInfo)
-    my_dir = kwargs['directory'] + "/" + ingest_date.strftime("%Y%m%d")
-    hdfs.make_dir(my_dir)
-    hdfs.make_dir(my_dir, permission=755)
 
-    os.chdir(output_path)
-    for file in os.listdir():
-        if file.endswith(".csv"):
-            file_path = f"{output_path}/{file}"
+    for subdir, dirs, files in os.walk(output_path):
+        for file in files:
+            print("file: " + file)
+            if file.endswith(".gitkeep"):
+                continue
 
+            folder_name = subdir.replace(output_path, "")
+            my_dir = kwargs['directory'] + "/" + \
+                ingest_date.strftime("%Y%m%d") + folder_name
+            print("my_dir: " + my_dir)
+            hdfs.make_dir(my_dir)
+            hdfs.make_dir(my_dir, permission=755)
+
+            file_path = os.path.join(output_path, subdir, file)
+            print(f"file path: {file_path}")
             with open(file_path, 'r', encoding="utf8") as file_data:
                 my_data = file_data.read()
                 hdfs.create_file(
                     my_dir+f"/{file}", my_data.encode('utf-8'), overwrite=True)
 
-    pprint("Stored! file: {}".format(file))
-    pprint(hdfs.list_dir(my_dir))
+                pprint("Stored! file: {}".format(file))
 
 
 with dag:
@@ -121,28 +127,27 @@ with dag:
         op_kwargs={'directory': '/data/raw_zone/moc_api'},
     )
 
-    # load_to_hdfs_for_redundant = PythonOperator(
-    #     task_id='load_to_hdfs_for_redundant',
-    #     python_callable=store_to_hdfs_for_redundant,
-    #     op_kwargs={'directory': '/data/raw_zone/moc_api'},
-    # )
+    load_to_hdfs_for_redundant = PythonOperator(
+        task_id='load_to_hdfs_for_redundant',
+        python_callable=store_to_hdfs_for_redundant,
+        op_kwargs={'directory': '/data/raw_zone/moc_api'},
+    )
 
-    # load_to_hdfs_processed = PythonOperator(
-    #     task_id='load_to_hdfs_processed',
-    #     python_callable=store_to_hdfs,
-    #     op_kwargs={'directory': '/data/processed_zone/moc_api'},
-    # )
+    load_to_hdfs_processed = PythonOperator(
+        task_id='load_to_hdfs_processed',
+        python_callable=store_to_hdfs,
+        op_kwargs={'directory': '/data/processed_zone/moc_api'},
+    )
 
-    # load_to_hdfs_processed_for_redundant = PythonOperator(
-    #     task_id='load_to_hdfs_processed_for_redundant',
-    #     python_callable=store_to_hdfs_for_redundant,
-    #     op_kwargs={'directory': '/data/processed_zone/moc_api'},
-    # )
+    load_to_hdfs_processed_for_redundant = PythonOperator(
+        task_id='load_to_hdfs_processed_for_redundant',
+        python_callable=store_to_hdfs_for_redundant,
+        op_kwargs={'directory': '/data/processed_zone/moc_api'},
+    )
 
     clean_up_output = BashOperator(
         task_id='clean_up_output',
         bash_command='rm -rf /opt/airflow/dags/output/moc_api/*',
     )
 
-ingestion_from_api >> load_to_hdfs >> clean_up_output
-# ingestion_from_api >> load_to_hdfs >> load_to_hdfs_for_redundant >> load_to_hdfs_processed >> load_to_hdfs_processed_for_redundant
+ingestion_from_api >> load_to_hdfs >> load_to_hdfs_for_redundant >> load_to_hdfs_processed >> load_to_hdfs_processed_for_redundant
